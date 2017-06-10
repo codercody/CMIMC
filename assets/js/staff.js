55cc0486a226b073e2aa004048b5d10ff142e275
Cody Johnson <ctj@Codys-MacBook-Pro.local>
2017-06-10 04:40:49 -0400

    staff page

---
+++ assets/js/staff.js
@@ -0,0 +1,142 @@
+var staff = [
+  {
+    name : "Cody Johnson",
+    photo : "cody-johnson.jpg",
+    description: "I do my work using a DFS: start a task, get distracted for a while, then come back and do the task at hand."
+  },
+  {
+    name : "Andrew Kwon",
+    photo : "andrew-kwon.jpg",
+    description: "A Korean."
+  },
+  {
+    name : "David Altizio",
+    photo : "david-altizio.jpg",
+    description: "My motto is that no geometry problem should be solved using coordinates."
+  },
+  {
+    name : "Patrick Lin",
+    photo : "patrick-lin.jpg",
+    description: "Wood VI."
+  },
+  {
+    name : "Victor Xu",
+    photo : "victor-xu.jpg",
+    description: "My name is Victor."
+  },
+  {
+    name : "Joshua Siktar",
+    photo : "joshua-siktar.jpg",
+    description: "I have gotten up at 7:30 every morning since moving in, including the weekends. Update: that lasted for three weeks."
+  },
+  {
+    name : "Taisuke Yasuda",
+    photo : "taisuke-yasuda.jpg",
+    description: "This year, I figured out my major."
+  },
+  {
+    name : "Emily Zhu",
+    photo : "emily-zhu.jpg",
+    description: "I think you should visit Booth (at Carnival!!)."
+  },
+  {
+    name : "Eugene Han",
+    photo : "eugene-han.jpg",
+    description: "Call me Gene."
+  },
+  {
+    name : "Tracy Wan",
+    photo : "tracy-wan.jpg",
+    description: "I am here for CMIMC."
+  },
+  {
+    name : "Annie Xu",
+    photo : "annie-xu.jpg",
+    description: "I like mudkip."
+  },
+  {
+    name : "Trajan Hammonds",
+    photo : "trajan-hammonds.jpg",
+    description: "I like math more than I like people."
+  },
+  {
+    name : "Tanuj Nayak",
+    photo : "tanuj-nayak.jpg",
+    description: "I wear long socks for a number of days that is approximately 1.5 times the number of days I wear ankle socks each year."
+  },
+  {
+    name : "Monica Pardeshi",
+    photo : "monica-pardeshi.jpg",
+    description: "I enjoy running and playing tennis."
+  },
+  {
+    name : "Sean Zhang",
+    photo : "sean-zhang.jpg",
+    description: "Do I have to put a quote?"
+  },
+  {
+    name : "David Morales",
+    photo : "david-morales.jpg",
+    description: "[]"
+  },
+  {
+    name : "Brian Scheuermann",
+    photo : "brian-scheuermann.jpg",
+    description: "I enjoy soccer."
+  },
+  {
+    name : "Katherine Zhang",
+    photo : "katherine-zhang.jpg",
+    description: "I like Star Wars."
+  },
+  {
+    name : "Darshan Chakrabarti",
+    photo : "darshan-chakrabarti.jpg",
+    description: "I like green."
+  },
+  {
+    name : "Frank Fan",
+    photo : "frank-fan.jpg",
+    description: "I enjoy you."
+  },
+  {
+    name : "Samuel Kim",
+    photo : "samuel-kim.jpg",
+    description: "Contrary to recent rumors, I am alive."
+  },
+  {
+    name : "Sam Li",
+    photo : "sam-li.jpg",
+    description: "Alright, cool."
+  },
+  {
+    name : "Alisa Chang",
+    photo : "alisa-chang.jpg",
+    description: "[]"
+  },
+  {
+    name : "Brendan Ney",
+    photo : "brendan-ney.jpg",
+    description: "#rememberTheSMT"
+  },
+  {
+    name : "Jianming Wang",
+    photo : "jianming-wang.jpg",
+    description: "I don't have a quote."
+  },
+  {
+    name : "Phillip Wang",
+    photo : "phillip-wang.jpg",
+    description: "Gang gang."
+  },
+  {
+    name : "Eric Nie",
+    photo : "eric-nie.jpg",
+    description: "My favorite color is blue."
+  },
+  {
+    name : "Ethan Gruman",
+    photo : "ethan-gruman.jpg",
+    description: "I am a true dog-lover."
+  }
+]
