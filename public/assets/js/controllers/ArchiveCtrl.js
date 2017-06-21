6f8cc60b6c5a1ec2e990e44f2504398fc9be0eeb
Cody Johnson <ctj@Codys-MacBook-Pro.local>
2017-06-21 14:28:06 -0400

    completed front end

--- public/assets/js/controllers/ArchiveCtrl.js
+++ public/assets/js/controllers/ArchiveCtrl.js
@@ -7,7 +7,7 @@ app.controller('ArchiveCtrl', [
       $scope.contest_problems = res.data.problems
       $scope.contest_results = res.data.results
       $scope.contest_photos = res.data.photos
-      $scope.do_something = function (x) {
+      $scope.commatize = function (x) {
         s = x[0]
         for (i = 1; i < x.length; i++)
           s += ", " + x[i];
